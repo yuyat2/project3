8f42a95567a33c64216c1a238d8c6960d37daa24
Yuya <inspiration1120@ruri.waseda.jp>
2018-02-19 16:53:30 -0800

    ggplot and time series analysis relations between corrruption and media censorship in China from 1950 to 2016

--- reproject3.R
+++ reproject3.R
@@ -20,6 +20,16 @@ fit <- lm(v2lgcrrpt ~ v2mecenefm, data = V_Dem_DS_CY_Others_v7_1)
 fit
 glance(fit)
 
+VDem_data <- V_Dem_DS_CY_Others_v7_1 %>%
+  filter(country_name == "China" & year %in% 1950:2016) %>%
+  select(year, v2lgcrrpt, v2jucorrdc, v2excrptps, v2mecenefm)
+
+
+
+ colnames(VDem_data)
+
+
+
 library("tidyverse")
 library("lubridate")
 library("stringr")
@@ -54,6 +64,18 @@ fit4 <- lm(v2exthftps~ v2mecenefm, data = V_Dem_DS_CY_Others_v7_1)
 fit4
 glance(fit4)
 
+
+
+
+
+
+china <- readRDS("china")
+read.csv(file = "/Users/yuya/reproject3/china")
+
+frequency(China)
+times_series <- ts.plot(china, col = : , xlab = "Year", ylab = "",
+                        main = "", start = 1950, frequency = )
+plot(times_series)
 
 #explantion of variables
 v2mecenefm government cesorship effort on media
